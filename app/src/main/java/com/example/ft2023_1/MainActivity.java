2f5fd2c2d9204fff7c3ba76444ab9b7cd2876581
Martas <martas.ambraziunas@gmail.com>
2023-02-20 19:32:09 +0200

    Revert "Added changeText button and implemented logic"
    This reverts commit 4baceb10

--- app/src/main/java/com/example/ft2023_1/MainActivity.java
+++ app/src/main/java/com/example/ft2023_1/MainActivity.java
@@ -3,23 +3,12 @@ package com.example.ft2023_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.tvMain = findViewById(R.id.tvMain);
-    }
-
-    public void onBtnChangeMainTextClick(View view) {
-        this.tvMain.setText("Text was changed!");
     }
 }
